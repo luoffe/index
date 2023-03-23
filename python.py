e3cb6eb1b6c9daa4be97e68936ccc8d8e9d135d3
luoffe <lisa.h@outlook>
2023-03-23 15:25:20 +0100

    Números de Python

--- python.py
+++ python.py
@@ -13,4 +13,44 @@ x = 6
 y = "John"
 print(x)
 print(y)
+# %% Números de Python
+#complex
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+# %% Conversión de tipo
+#complex, float, int
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+print(x)
+print(y)
+print(z)
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+# %% Número aleatorio
+import random
+
+print(random.randrange(1, 10))
 # %%
