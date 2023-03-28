436678074f4ae2ab24be06c7882beb3681e9c259
luoffe <lisa.h@outlook>
2023-03-28 15:55:48 +0200

    MTC: page 404

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g$&_a%b=ypbgiz!(dsi6&oxfk%48u3c_wefii26m@8nculcb*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
