e8f5f4577164029d8e7953bada8a94e5db5a9bb9
luoffe <lisa.h@outlook>
2023-03-28 16:31:25 +0200

    MTC: page tedting, pruebas mantener del admin

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,4 @@
+# Create your models here.
 from django.db import models
 
 class Member(models.Model):
@@ -5,4 +6,6 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
-# Create your models here.
+
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
